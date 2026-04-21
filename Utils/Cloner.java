188f28493d3e048114df7ea61bbcdbf4737aa33e
7amoudi911 <mahmoudaamem@gmail.com>
2026-04-22 00:30:58 +0200

    Update Cloner.java

--- Utils/Cloner.java
+++ Utils/Cloner.java
@@ -1,1 +1,23 @@
+package utils;
+
+import java.util.*;
+import Models.Process;
+
+public class Cloner {
+
+    public static List<Process> cloneList(List<Process> original) {
+        List<Process> copy = new ArrayList<>();
+
+        for (Process p : original) {
+            copy.add(new Process(
+                p.getId(),
+                p.getArrivalTime(),
+                p.getBurstTime(),
+                p.getPriority()
+            ));
+        }
+
+        return copy;
+    }
+}
 
