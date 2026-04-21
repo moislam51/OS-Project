199b5f31b66c6b9ba422fba439dcf1a1c854a2da
moislam51 <mohamedislamshaker511@gmail.com>
2026-04-21 22:54:51 +0200

    Update Process.java

--- Models/Process.java
+++ Models/Process.java
@@ -1,1 +1,24 @@
+public class Process {
+    public String id;
+    public int arrivalTime;
+    public int burstTime;
+    public int priority;
+    
+    
+    public int remainingTime;
+    public int startTime = -1;
+    public int completionTime;
+    
+    public int waitingTime = 0;
+    public int turnaroundTime = 0;
+    public int responseTime = 0;
 
+    public Process(String id, int at, int bt, int pr) {
+        this.id = id;
+        this.arrivalTime = at;
+        this.burstTime = bt;
+        this.priority = pr;
+        this.remainingTime = bt;
+        System.out.println("object created");
+    }
+}
