d5ed9175637dc528e6d2926977d3c2697ec24a81
moislam51 <mohamedislamshaker511@gmail.com>
2026-04-22 17:35:54 +0200

    Final Result.java

--- Models/Result.java
+++ Models/Result.java
@@ -1,1 +1,25 @@
 
+package models;
+import java.util.*;
+
+public class Result {
+
+    
+    public List<String> ganttChart = new ArrayList<>();
+    public List<Process> processes = new ArrayList<>();
+
+ 
+    public double avgWT;   
+    public double avgTAT; 
+    public double avgRT; 
+
+
+    public Result(List<String> ganttChart, List<Process> processes,
+                  double avgWT, double avgTAT, double avgRT) {
+        this.ganttChart = ganttChart;
+        this.processes = processes;
+        this.avgWT = avgWT;
+        this.avgTAT = avgTAT;
+        this.avgRT = avgRT;
+    }
+}
